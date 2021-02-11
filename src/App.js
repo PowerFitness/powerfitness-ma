f5787b044ab579197134176173119e697616d473
@cjscholl <catelynjscholl@gmail.com>
2021-02-10 18:52:20 -0600

    feat: initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.div`
+    background-color: lightblue;
+    color: blue;
+`;
+
+export const App = () => <Header>
+    Hi
+</Header>
+
+export default App;
