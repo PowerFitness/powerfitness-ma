733f67b1b7ce4cf407c77acb82e80bdce4b1deb6
@cjscholl <catelynjscholl@gmail.com>
2021-02-21 18:01:32 -0600

    feat: more initial setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import {hot} from "react-hot-loader";
-
-const Header = styled.div`
-    background-color: lightblue;
-    color: green;
-`;
+import {   BrowserRouter as Router, Route } from 'react-router-dom';
+import { hot } from "react-hot-loader";
+import Header from './components/Header';
 
 export const App = () => <Header>
-    Hi
+    <Router>
+        <Route path="/" />
+        <Route path="/plan" />
+        <Route path="/dashboard" />
+        <Route path="/journal" />
+    </Router>
 </Header>
 
 export default hot(module)(App);
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.div`
+    background-color: lightblue;
+    color: green;
+`;
+
+export const Header = () => <StyledHeader>PowerFitness</StyledHeader>
+
+export default Header;
