bf90b55d63d4f0c434aabe5e707f5c961a998138
Pramiti91 <pramiti.chakraborty@gmail.com>
2021-02-25 20:15:52 -0600

    frontend development changes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 import {   BrowserRouter as Router, Route } from 'react-router-dom';
 import { hot } from "react-hot-loader";
+import {PlanPage} from './components/PlanPage';
+import {JournalPage} from './components/JournalPage';
+import {SplashPage} from './components/SplashPage';
+import {DashboardPage} from './components/DashboardPage';
+import {MyAccountPage} from './components/MyAccountPage';
 import Header from './components/Header';
 
-export const App = () => <Header>
-    <Router>
-        <Route path="/" />
-        <Route path="/plan" />
-        <Route path="/dashboard" />
-        <Route path="/journal" />
+export const App = () => 
+<div>
+        <Router>
+            <Header></Header>
+        <Route path="/" component={SplashPage}/>
+        <Route path="/dashboard" component={DashboardPage}/>
+        <Route path="/plan" component={PlanPage}/>
+        <Route path="/journal" component={JournalPage}/>
+        <Route path="/myAccount" component={MyAccountPage}/>
     </Router>
-</Header>
+    </div>
 
 export default hot(module)(App);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
+import {Link, useLocation} from 'react-router-dom';
 
 const StyledHeader = styled.div`
-    background-color: lightblue;
-    color: green;
+padding: 26px 70px;
+background-color: #FFF;
+box-shadow: 0 2px 2px 0 rgba(0,0,0,0.18);
+display: flex;
+flex-direction: row;
+`;
+const StyledLink = styled(Link)`
+height: 22px;
+  color: #0081AF;
+  font-size: 22px;
+  font-weight: 300;
+  line-height: 22px;
+  margin-right: 44px;
+  text-decoration: ${props=>props.selected ? "underline" : "none"};
+  &[data-active] {
+    color: red;
+  }
+`;
+const Logo = styled(StyledLink)`
+  font-weight: 700;
+`;
+const Account = styled(StyledLink)`
+ margin-left: auto;
+ margin-right: 0;
 `;
 
-export const Header = () => <StyledHeader>PowerFitness</StyledHeader>
+export const Header = () => {
+    const { pathname } = useLocation(); 
 
+    return (<StyledHeader>
+    <Logo to="/dashboard">PowerFitness</Logo>
+    <StyledLink selected={pathname=='/dashboard'}  to="/dashboard">Dashboard</StyledLink>
+    <StyledLink selected={pathname=='/plan'} to="/plan">Fitness Planning</StyledLink>
+    <StyledLink selected={pathname=='/journal'} to="/journal">Daily Journal</StyledLink>
+    <Account selected={pathname=='/myAccount'} to="/myAccount">My Account</Account>
+    </StyledHeader>)
+}
+    
 export default Header;
