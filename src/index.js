913cacb78046cff77b604790ef8e2ee9c721d6c9
@cjscholl <catelynjscholl@gmail.com>
2021-03-01 19:26:14 -0600

    feat: add redux thunks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
