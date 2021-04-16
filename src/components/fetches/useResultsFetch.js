e38eafe8aca1535d0209f375d72308feb6a26cd1
Fawzieh Assad <fawziehassad@gmail.com>
2021-04-15 19:46:49 -0500

    store selected date into state

--- src/components/fetches/useResultsFetch.js
+++ src/components/fetches/useResultsFetch.js
@@ -1,12 +1,13 @@
 import { useSelector } from 'react-redux';
 import * as userSelectors from '../../selectors/userSelectors';
+import * as dateSelector from '../../selectors/dateSelector';
 import { useFetch } from './useFetch';
 
-export const fetchResultsUrl = (userUniqueId, date) => `/api/powerfitness/results?userUniqueId=${userUniqueId}`
+export const fetchResultsUrl = (userUniqueId, date) => `/api/powerfitness/results?userUniqueId=${userUniqueId}&date=${date}`
 
 export const useResultsFetch = () => {
     const userUniqueId = useSelector(userSelectors.userUniqueId);
-    const date = useSelector(userSelectors.date);
-    const useFetched = useFetch(fetchResultsUrl(userUniqueId), 'results')
-    return useFetched
+    const date = useSelector(dateSelector.selectedDate);
+    const useFetched = useFetch(fetchResultsUrl(userUniqueId, date), 'results');
+    return useFetched;
 }
