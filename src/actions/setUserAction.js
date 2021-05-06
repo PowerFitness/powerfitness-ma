add06dc0b83a011f201dc2ad7bbc9a4ce20b4940
@cjscholl <catelynjscholl@gmail.com>
2021-05-05 22:39:37 -0500

    fix: close sign in model on auth change

--- src/actions/setUserAction.js
+++ src/actions/setUserAction.js
@@ -1,7 +1,11 @@
 import { firebase } from '../utils/firebase';
+import * as uiActions from './uiActions';
 
 export const setUserAction = (user) => ({ type: 'setUserAction', payload: user });
 
 export const observeLogin = (dispatch) => firebase.auth().onAuthStateChanged(
-    (user) => dispatch(setUserAction(user || { loggedOut: true }))
+    (user) => {
+        dispatch(setUserAction(user || { loggedOut: true }))
+        dispatch(uiActions.setUiAction({ isSignInOpen: false }));
+    }
 );
