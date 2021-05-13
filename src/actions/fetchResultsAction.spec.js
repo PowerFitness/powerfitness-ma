65401c6945f4e5bbc34863dc16dee12a98195246
Pramiti91 <pramiti.chakraborty@gmail.com>
2021-05-12 21:51:17 -0500

    Test for fetchResultsActions

--- src/actions/fetchResultsAction.spec.js
+++ src/actions/fetchResultsAction.spec.js
@@ -3,7 +3,8 @@ import * as fetchResultsActions from './fetchResultsAction';
 describe('fetchResultsActions', () => {
     test('refetchResults', () => {
         const userUniqueId = 'abc';
-        expect(fetchResultsActions.refetchResults(userUniqueId)).toEqual(
+        const date = '2021-12-31';
+        expect(fetchResultsActions.refetchResults(userUniqueId, date)).toEqual(
             {
                 type:'refetch/results',
                 payload: {
